6e38502c8b7ae68ef1b8194769501624553e0f99
Kanishk-kumar399 <kanishkk301198@gmail.com>
2020-10-26 12:54:02 +0530

    [Kanishk] added :TestCase2 Given the State Census CSV File if incorrect Returns a custom Exception

--- src/main/java/com/indiancensusanalyzer/CensusAnalyserException.java
+++ src/main/java/com/indiancensusanalyzer/CensusAnalyserException.java
@@ -4,7 +4,7 @@ public class CensusAnalyserException extends Exception
 {
 	enum CensusExceptionType
 	{
-        CENSUS_FILE_PROBLEM
+        CENSUS_FILE_PROBLEM, INCORRECT_DATA_ISSUE, SOME_OTHER_INPUTOOTPUT_EXCEPTION, DELIMITER_ISSUE,  NO_SUCH_CLASS
     }
     CensusExceptionType type;
     public CensusAnalyserException(CensusExceptionType type, String message) {
--- src/test/java/com/indiancensusanalyzer/StateCensusAnalyserTest.java
+++ src/test/java/com/indiancensusanalyzer/StateCensusAnalyserTest.java
@@ -2,13 +2,15 @@ package com.indiancensusanalyzer;
 
 import static org.junit.Assert.assertTrue;
 
+import java.io.IOException;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class StateCensusAnalyserTest 
 {
 	public static final String CENSUS_CSV_FILE = "C:\\Users\\Asus\\eclipse-workspace\\IndianStateCensusAnalyser\\src\\main\\resources\\IndiaCSVCensusData.csv";
-	
+	public static final String INCORRECT_FILE = "useless.csv";
 	@Test
 	public void givenIndiaCensusDataCsvShouldReturnExactCount() {
 		StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
@@ -20,5 +22,18 @@ public class StateCensusAnalyserTest
 			e.printStackTrace();
 		}
 
+	}
+	@Test
+	public void givenCensusDataCsv_IfNotExist_ShouldthrowCensusAnalyserException()
+	{
+		StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+		try {
+			stateCensusAnalyser.loadStateCensusData(INCORRECT_FILE);
+		}
+		catch (CensusAnalyserException e) 
+		{
+			e.printStackTrace();
+			 Assert.assertEquals(CensusAnalyserException.CensusExceptionType.CENSUS_FILE_PROBLEM, e.type);
+		}
 	}
 }
