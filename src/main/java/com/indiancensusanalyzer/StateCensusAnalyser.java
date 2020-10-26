ca89140036c2683610bff6a5990b1c73a01c208f
Kanishk-kumar399 <kanishkk301198@gmail.com>
2020-10-26 23:08:09 +0530

    [Kanishk] refactored:1A for solving dry run error

--- src/main/java/com/indiancensusanalyzer/StateCensusAnalyser.java
+++ src/main/java/com/indiancensusanalyzer/StateCensusAnalyser.java
@@ -21,47 +21,21 @@ public class StateCensusAnalyser
 		try {	
 			Reader reader;
 			reader = Files.newBufferedReader(Paths.get(csvfilePath));
-			CsvToBeanBuilder<CSVStateCensus> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
-			csvToBeanBuilder.withType(CSVStateCensus.class);
-			csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
-			CsvToBean<CSVStateCensus> csvToBean = csvToBeanBuilder.build();
-			Iterator<CSVStateCensus> csvStateCensusIterator = csvToBean.iterator();
+			Iterator<CSVStateCensus> csvStateCensusIterator = getIteratorCSV(reader,CSVStateCensus.class);
 			Iterable<CSVStateCensus> csvStateCensusIterable = () -> csvStateCensusIterator;
 			int numberOfEntries = (int) StreamSupport.stream(csvStateCensusIterable.spliterator(), false).count();
 			return numberOfEntries;
-		} 
+			} 
 		catch (IOException e) {
 			throw new CensusAnalyserException(CensusAnalyserException.CensusExceptionType.CENSUS_FILE_PROBLEM,"Incorrect File");
 		}
-		catch (RuntimeException e) 
-		{
-			if (ExceptionUtils.indexOfType(e, CsvDataTypeMismatchException.class) != -1)
-			{
-				throw new CensusAnalyserException(CensusAnalyserException.CensusExceptionType.INCORRECT_TYPE_ISSUE,"Incorrect Type");
-			} 
-			else if (ExceptionUtils.indexOfType(e, CsvRequiredFieldEmptyException.class) != -1) 
-			{
-				if(e.getMessage().equalsIgnoreCase("Error capturing CSV header")) {
-					throw new CensusAnalyserException(CensusExceptionType.INCORRECT_HEADER,"Incorrect header");
-				}else {
-					throw new CensusAnalyserException(CensusExceptionType.DELIMITER_ISSUE,"Incorrect Delimiter Issue");
-				}
-			} 
-			else {
-				e.printStackTrace();
-				throw new RuntimeException();
-			}
 	}
-	}
-	public int loadIndianStateData(String csvfilePath) throws CensusAnalyserException {
+	public int loadIndianStateData(String csvfilePath) throws CensusAnalyserException 
+	{
 		try {	
 			Reader reader;
 			reader = Files.newBufferedReader(Paths.get(csvfilePath));
-			CsvToBeanBuilder<CSVStates> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
-			csvToBeanBuilder.withType(CSVStates.class);
-			csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
-			CsvToBean<CSVStates> csvToBean = csvToBeanBuilder.build();
-			Iterator<CSVStates> csvStateIterator = csvToBean.iterator();
+			Iterator<CSVStates> csvStateIterator = getIteratorCSV(reader,CSVStates.class);
 			Iterable<CSVStates> csvStateIterable = () -> csvStateIterator;
 			int numberOfEntries = (int) StreamSupport.stream(csvStateIterable.spliterator(), false).count();
 			return numberOfEntries;
@@ -69,6 +43,17 @@ public class StateCensusAnalyser
 		catch (IOException e) {
 			throw new CensusAnalyserException(CensusAnalyserException.CensusExceptionType.CENSUS_FILE_PROBLEM,"Incorrect File");
 		}
+	}
+	public<T> Iterator<T> getIteratorCSV(Reader reader, Class<T> CSVclass) throws CensusAnalyserException 
+	{
+		try {
+			CsvToBeanBuilder<T> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+			csvToBeanBuilder.withType(CSVclass);
+			csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+			CsvToBean<T> csvToBean = csvToBeanBuilder.build();
+			Iterator<T> censusCSVIterator=csvToBean.iterator();
+			return censusCSVIterator;
+		}
 		catch (RuntimeException e) 
 		{
 			if (ExceptionUtils.indexOfType(e, CsvDataTypeMismatchException.class) != -1)
@@ -77,16 +62,19 @@ public class StateCensusAnalyser
 			} 
 			else if (ExceptionUtils.indexOfType(e, CsvRequiredFieldEmptyException.class) != -1) 
 			{
-				if(e.getMessage().equalsIgnoreCase("Error capturing CSV header")) {
+				if(e.getMessage().equalsIgnoreCase("Error capturing CSV header!")) {
 					throw new CensusAnalyserException(CensusExceptionType.INCORRECT_HEADER,"Incorrect header");
-				}else {
+				}
+				else 
+				{
 					throw new CensusAnalyserException(CensusExceptionType.DELIMITER_ISSUE,"Incorrect Delimiter Issue");
 				}
 			} 
 			else {
+				System.out.println(e.getMessage());
 				e.printStackTrace();
-				throw new RuntimeException();
+				throw e;
 			}
-	}
+		}
 	}
 }
