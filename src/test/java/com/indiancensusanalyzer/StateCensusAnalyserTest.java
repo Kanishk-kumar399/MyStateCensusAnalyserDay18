c780c222d68d409af615344101165fb556b123b3
Kanishk-kumar399 <kanishkk301198@gmail.com>
2020-10-26 12:15:48 +0530

    Added

--- src/test/java/com/indiancensusanalyzer/StateCensusAnalyserTest.java
+++ src/test/java/com/indiancensusanalyzer/StateCensusAnalyserTest.java
@@ -7,13 +7,13 @@ import org.junit.Test;
 
 public class StateCensusAnalyserTest 
 {
-	public static final String CENSUS_CSV_FILE = "src/main/resources/IndiaCSVCensusData.csv";
+	public static final String CENSUS_CSV_FILE = "/src/main/resources/IndiaCSVCensusData.csv";
 	@Test
 	public void givenIndiaCensusDataCsvShouldReturnExactCount() {
 		StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
 		try {
 			int numberOfEntries = stateCensusAnalyser.loadStateCensusData(CENSUS_CSV_FILE);
-			Assert.assertEquals(6,numberOfEntries);
+			Assert.assertEquals(10,numberOfEntries);
 			} 
 		catch (CensusAnalyserException e) {
 			e.printStackTrace();
